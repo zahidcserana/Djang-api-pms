47a73acf01a46a585fe2eed9f9be22bd26ca5060
zahidcserana <zahid1004048@gmail.com>
2020-04-10 21:31:00 +0600

    static

--- tutorial/wsgi.py
+++ tutorial/wsgi.py
@@ -6,20 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
-#
-# import os
-#
-# from django.core.wsgi import get_wsgi_application
-#
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutorial.settings')
-#
-# application = get_wsgi_application()
-
 
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tutorial.settings")
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutorial.settings')
+
+application = get_wsgi_application()
