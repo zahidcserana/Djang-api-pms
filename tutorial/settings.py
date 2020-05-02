54fe934e99360fe527985844bd7ab62c45c42a7a
zahidcserana <zahid1004048@gmail.com>
2020-05-02 23:24:48 +0600

    postgresql

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -11,14 +11,9 @@ import os
 import django_heroku
 from decouple import config
 import dj_database_url
-import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# This is new:
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
